7d86097345dbd0a564070a5810454ae197022302
1510749 <31429765+1510749@users.noreply.github.com>
2018-12-15 16:07:56 -0800

    Update Classes.h

--- Classes.h
+++ Classes.h
@@ -1,4 +1,3 @@
-
 #include <string>
 #include <vector>
 
@@ -11,10 +10,10 @@ protected:
 	//can add other stats later once we actually finish the game or the project is over lol
 public:
 	//basic methods for the health
-	void setHealth(int x) { health = x; }
-	int getHealth() { return health; }
-	void addHealth(int x) { health += x; }
-	void subHealth(int x) { health -= x; }
+	void setHealth(int x) { health = x; };
+	int  getHealth()      { return health; };
+	void addHealth(int x) { health += x; };
+	void subHealth(int x) { health -= x; };
 };
 
 class Character : public Entities {
@@ -22,21 +21,30 @@ protected:
 	string weaponEquipped;
 	int wepDmg;
 	vector<string> inventory = {};
-	//values for either item damage or -999 if it is a consumable(like a code to know if it is a consumable)
-	//ideally we'll probably want item IDs but who cares for this project at the moment
-	vector<int> itemValues = {};
+	//ideally we'll probably want item IDs-- thats what we'll do
+	//Types:  0 - misc    1 - weapon    2 - consumable
+	vector<int> itemType = {};
 public:
 	void printInventory();
+	int selectItem(string x);
 	void equipItem(int x);
 };
 
-//so we can have item descriptions, can do same for weapons too if we want
-class Consumables {
+
+
+//-----------------------------------------------------------------------------------------------------------
+//so we can have item descriptions, can do same for weapons too if we want; we actually should
+//I have it all together in case we want consumables that do damage.
+class Items {
 protected:
+	const int TYPE;
 	int hpValue;
+	int dmgValue;
 	string description;
 public:
-	int gethpValue() { return hpValue; }
-	void printDesc();
+	int getType() { return TYPE; };
+	int gethpValue() { return hpValue; };
+	int getdmgValue() { return dmgValue; };
+	void printDesc(int y);
 };
 
